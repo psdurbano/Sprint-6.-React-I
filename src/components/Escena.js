df2ea514e4c6ea07de9c2bbb5b88a919e43a6f07
Pablo <pablosdurbano@gmail.com>
2023-02-01 10:31:31 +0100

    Exercise 1

---
+++ src/components/Escena.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Text() {
+  return (
+    <div>
+      <h1>
+        El nostre heroi estava surant per l'espai sideral quan a la llunyania va
+        albirar una nau espacial
+      </h1>
+    </div>
+  );
+}
