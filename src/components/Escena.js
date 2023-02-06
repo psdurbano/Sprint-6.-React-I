aa9dfab22e6d3b76eade24137ca0abc0c8cc4005
Pablo <pablosdurbano@gmail.com>
2023-02-06 12:46:05 +0100

    Exercice 3 and 4 halfway

--- src/components/Escena.js
+++ src/components/Escena.js
@@ -1,11 +1,16 @@
 import React from "react";
 import data from "./data.json";
-
+import { Caja, Styledbutton } from "./button.js";
+import { Boton } from "./button.js";
 export default function Text() {
   return (
     <div>
+      <Boton>
+        <Styledbutton>Anterior</Styledbutton>
+        <Styledbutton>Siguiente</Styledbutton>
+      </Boton>
       {data.map((frase) => {
-        return <p key={frase}>{frase}</p>;
+        return <Caja key={frase}>{frase}</Caja>;
       })}
     </div>
   );
---
+++ src/components/button.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Caja = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: white;
+  font-size: 0, 5em;
+  margin: 8px auto 8px auto;
+  padding: 5px;
+  border: 2px solid black;
+  border-radius: 15px;
+  width: 95%;
+`;
+
+export const Boton = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: white;
+  font-size: 0, 5em;
+  margin: 8px auto 8px auto;
+  padding: 10px;
+  width: 95%;
+`;
+export const Styledbutton = styled.button`
+  font-size: 2em;
+  width: 100%;
+`;
