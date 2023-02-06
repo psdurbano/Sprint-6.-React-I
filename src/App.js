216ff1fb79878ccab1d0f508aa2711178f54ac59
Pablo <pablosdurbano@gmail.com>
2023-02-06 09:45:09 +0100

    Exercici 2

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ function App() {
   return (
     <div>
       <Text />
-      <Text />
-      <Text />
-      <Text />
     </div>
   );
 }
--- src/components/Escena.js
+++ src/components/Escena.js
@@ -1,16 +1,11 @@
 import React from "react";
-export default function Text() {
-  const Frases = [
-    "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
-    "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
-    "L'heroi va decidir travessar la porta que el portava a casa",
-    "Mentrestant, altres herois no van tenir tanta sort en la seva elecció ...",
-  ];
+import data from "./data.json";
 
+export default function Text() {
   return (
     <div>
-      {Frases.map((frase) => {
-        <p>{frase}</p>
+      {data.map((frase) => {
+        return <p key={frase}>{frase}</p>;
       })}
     </div>
   );
